c575983d16fcd6872f4a29e2b48b685e1cdbae19
EC2 Default User <ec2-user@ip-172-31-48-210.ec2.internal>
2023-11-21 15:16:56 +0000

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Predict US stocks closing movements - Optiver Kaggle Competition 2023',
+    author='MAKONG Ludovic',
+    license='',
+)
